aaf384da278b8b013d966fc26b887f594546ecd0
DanSizov <111688811+DanSizov@users.noreply.github.com>
2022-10-11 19:08:30 +0300

    the tail grows now

--- chernovik.c
+++ chernovik.c
@@ -6,15 +6,22 @@
 #define width 25
 #define height 15
 
-int numberoftails = 0;
-int i = 0;
+
+int i = 1;
+
+int x, y;
+
 char map[height][width+1];
-int bufx, bufy;
-int buftailx, buftaily;
+
 int foodCounter = 0;
-int foodCounterBuf = 0;
+
 char key;
+
 int direction;
+
+int tailpos;
+
+int game = 0;
 
 typedef struct
 {
@@ -24,27 +31,30 @@ typedef struct
 
 typedef struct
 {
-	int head_x, head_y, head_w;
+	int head_x, head_y;
 	
 } TypeSnake;
 
-TypeSnake snake;
-
 typedef struct
 {
-	int tail_x, tail_y, tail_w;
-	int buftailx1, buftaily1;
+	int tail_x, tail_y;
+	int buftailx, buftaily;
+	
 } TypeTail;
 
-//struct TypeTail* tl;
-
 TypeFood food;
+TypeSnake snake;
+TypeTail tail[100];
 
-TypeTail tail;
+void gameOver()
+{		
+	system("cls");
+	game = 1;
+	
+	printf("GAME OVER\n\n\n\n");
+}
 
 void initmap() {
-	
-	//tl = (struct TypeTail*)malloc(i * sizeof(TypeTail));
 
 	for (int i = 0; i < width; i++)
 	{
@@ -81,63 +91,55 @@ int noblock() {
 
 void moveSnakeAndTail() {
 	
-	key = noblock();
-	tail.buftailx1 = snake.head_x;
-	tail.buftaily1 = snake.head_y;
 
-	if ((key == 'w' || key == 's' || key == 'a' || key == 'd') && (abs(direction - key) > 5))
-		direction = key;
+		key = noblock();
+		
+		tail[0].buftailx = snake.head_x;
+		tail[0].buftaily = snake.head_y;
 
-	if (direction == 'w')
-		snake.head_y--;
-	if (direction == 's')
-		snake.head_y++;
-	if (direction == 'a')
-		snake.head_x--;
-	if (direction == 'd')
-		snake.head_x++;
+		for (int i = 1; i < foodCounter; i++) {
+		
+			tail[i + 1].buftailx = tail[i].tail_x;
+			tail[i + 1].buftaily = tail[i].tail_y;
+		}
 
-	if (snake.head_x >= 24)
-		snake.head_x = 1;
+		if ((key == 'w' || key == 's' || key == 'a' || key == 'd') && (abs(direction - key) > 5))
+			direction = key;
 
-	if (snake.head_x <= 0)
-		snake.head_x = 23;
+		if (direction == 'w')
+			snake.head_y--;
 
-	if (snake.head_y >= 14)
-		snake.head_y = 1;
+		if (direction == 's')
+			snake.head_y++;
 
-	if (snake.head_y <= 0)
-		snake.head_y = 13;
+		if (direction == 'a')
+			snake.head_x--;
 
-	tail.tail_x = tail.buftailx1;
-	tail.tail_y = tail.buftaily1;
+		if (direction == 'd')
+			snake.head_x++;
 
-	//buftailx = tail.tail_x;
-	//buftaily = tail.tail_y;
+		if (snake.head_x >= 24)
+			snake.head_x = 1;
 
+		if (snake.head_x <= 0)
+			snake.head_x = 23;
 
+		if (snake.head_y >= 14)
+			snake.head_y = 1;
+
+		if (snake.head_y <= 0)
+			snake.head_y = 13;
 }
 
 void initSnakeAndTail() {
 
 		snake.head_x = width / 2;
 		snake.head_y = height / 2;
-		snake.head_w = 1;
-		tail.tail_x = tail.buftailx1;
-		tail.tail_y = tail.buftaily1;
-		tail.tail_w = 1;
-	
 }
 
 void putSnake() {
 
 	map[snake.head_y][snake.head_x] = 'S';
-
-}
-
-void growTail() {
-	
-
 }
 
 void initFood() {
@@ -149,56 +151,46 @@ void initFood() {
 void putFood() {
 
 	map[food.pos_y][food.pos_x] = '*';
-	
 
-
-	foodCounterBuf = foodCounter;
 		if ((snake.head_x == food.pos_x) && (snake.head_y == food.pos_y))
 		{
-
 			food.pos_x = 1 + rand() % 23;
 			food.pos_y = 1 + rand() % 13;
 			foodCounter++;
 			
 		}
+		for (int i = 0; i < foodCounter; i++) {
 
-		if (foodCounter >=1 ) {
-			map[tail.tail_y][tail.tail_x] = 's';
+				tail[i + 1].buftailx = tail[i].tail_x;
+				tail[i + 1].buftaily = tail[i].tail_y;
 
-			tail.tail_x = tail.buftailx1;
-			tail.tail_y = tail.buftaily1;
-			tail.tail_w = 1;
-		};
-		//if (foodCounter >= 2) {
-		//	map[tail.tail_y][tail.tail_x] = 's';
+				tail[i].tail_x = tail[i].buftailx;
+				tail[i].tail_y = tail[i].buftaily;
 
-		//	tail.tail_x = tail.buftailx1;
-		//	tail.tail_y = tail.buftaily1;
-		//	tail.tail_w = 1;
-		//};
-
-	
+				map[tail[i].tail_y][tail[i].tail_x] = 's';
+		}
+		if (map[snake.head_y][snake.head_x] == map[tail[i].tail_y][tail[i].tail_x])
+			gameOver();
 }
-
 
 void main()
 {
 		initSnakeAndTail();
 		initFood();
 		
-		do {
-		system("cls");
-		initmap();
-		moveSnakeAndTail();
-		putFood();
+		 while (game == 0) {
+			system("cls");
+			initmap();
+			moveSnakeAndTail();
+			putFood();
+			putSnake();
+			showMap();
+			Sleep(75);
 
-		putSnake();;
-		showMap();
-		
-		Sleep(75);
-			}
-		while (GetKeyState(VK_ESCAPE) >= 0);
-
+		 if (GetKeyState(VK_ESCAPE) < 0)
+		 {
+			 gameOver();
+		 }
+							}
 		return 0;
-		
 	}
